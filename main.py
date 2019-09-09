3b6065732995ff29be2777612d624a2006c0c32c
sudo0272 <sudo0272@gmail.com>
2019-09-09 23:33:04 +0900

    read file and added to candidates

--- main.py
+++ main.py
@@ -7,9 +7,25 @@ target = open(targetFile, 'rb')
 
 data = list(i.split('\\') for i in db.readlines())
 
-sample = target.read(100)  # max length of file signature
-                           # MSC
-                           # MMC Snap-in Control file
-                           # 3C3F786D6C2076657273696F6E3D22312E30223F3E0D0A3C4D4D435F436F6E736F6C6546696C6520436F6E736F6C65566572
+# print(data)
+
+sample = target.read(50).hex()  # max length of file signature
+                                # MSC
+                                # MMC Snap-in Control file
+                                # 3C3F786D6C2076657273696F6E3D22312E30223F3E0D0A3C4D4D435F436F6E736F6C6546696C6520436F6E736F6C65566572
 sampleLength = len(sample)
 
+# print(sample)
+
+candidates = []
+
+for i in range(len(data)):
+    signatureLength = len(data[i][1])
+    
+    if signatureLength <= sampleLength:
+        for j in range(signatureLength):
+            if data[i][1][j] != sample[j]:
+                break
+        
+        if j == signatureLength - 1:  # if first part is same
+            candidates.append(i)
