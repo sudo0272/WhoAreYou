592571fa4ee6b51918cb0f26f3f47b225bceb925
sudo0272 <sudo0272@gmail.com>
2019-09-11 14:06:43 +0900

    crawl data if extensions.txt not found

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import sys
+from urllib.request import urlopen
+from urllib.error import *
+from bs4 import BeautifulSoup
 
 try:
     targetFile = sys.argv[1]
@@ -10,7 +13,36 @@ try:
     db = open('./extensions.txt')
 except FileNotFoundError:
     print('extensions.txt not found')
-    sys.exit()
+    print('Crawling data')
+    try:
+        signatures = []
+
+        db = open('extensions.txt', 'w')
+
+        for i in range(1, 19):
+            url = 'https://filesignatures.net/index.php?page=all&order=EXT&alpha=&currentpage=%d' % i
+
+            webpage = urlopen(url)
+
+            source = BeautifulSoup(webpage, 'html5lib').find('table', {'id': 'innerTable'}).find('tbody')
+
+            for i in source.findChildren('tr'):
+                t = list(j.get_text().strip() for j in i.findChildren('td'))
+
+                t.pop(0)
+
+                if t[0] != 'Extension':
+                    t[1] = ''.join(t[1].split(' '))
+                    
+                    db.write('\\'.join(t))
+                    db.write('\n')
+
+        db.close()
+    except URLError:
+        print('Internet not connected')
+        sys.exit()
+
+
 
 try:
     target = open(targetFile, 'rb')
